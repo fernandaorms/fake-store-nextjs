f429c2f84274e793f1544d37f30a951845f93216
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-11-30 11:15:59 -0300

    update capitalize

--- src/app/search/layout.js
+++ src/app/search/layout.js
@@ -6,7 +6,7 @@ export default function SearchLayout({ children }) {
     return (
         <>
             <div className='mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white'>
-                <div className='order-first w-full flex-none md:max-w-[125px]'>
+                <div className='capitalize order-first w-full flex-none md:max-w-[125px]'>
                     <Categories />
                 </div>
 
@@ -14,7 +14,7 @@ export default function SearchLayout({ children }) {
                     {children}
                 </div>
 
-                <div className='order-none flex-none md:order-last md:w-[125px]'>
+                <div className='capitalize order-none flex-none md:order-last md:w-[125px]'>
                     <FilterList list={sorting} title='Sort by' />
                 </div>
             </div>
