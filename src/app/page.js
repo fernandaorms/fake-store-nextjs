9fd0462ec72b6bd35da017f5695638250dd445f7
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-12-01 09:59:47 -0300

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { ThreeItemGrid } from "@/components/grid/three-items";
 import { Carousel } from "@/components/carousel";
+import Footer from "@/components/layout/footer";
 
 export default function Home() {
     return (
         <>
             <ThreeItemGrid />
             <Carousel />
+            <Footer />
         </>
     );
 }
